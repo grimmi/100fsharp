6f03dcf7951b2b58244766f6e2035c5d029aef83
Arne Lehmann <arne.lehmann@gmx.de>
2017-04-29 12:57:01 +0200

    changed fizzbuzz to use tuples

--- fizzbuzz.fsx
+++ fizzbuzz.fsx
@@ -1,14 +1,12 @@
 let buzzer n = 
-    match n with
-    | n when n % 3 = 0 && n % 5 = 0 -> printfn "fizzbuzz"
-    | n when n % 3 = 0 -> printfn "fizz"
-    | n when n % 5 = 0 -> printfn "buzz"
+    match (n % 3, n % 5) with
+    | (0,0) -> printfn "fizzbuzz"
+    | (0,_) -> printfn "fizz"
+    | (_,0) -> printfn "buzz"
     | _ -> printfn "%d" n
 
 let fizzbuzz n =
-    let range = [ 1 .. n ]
-
-    range
+    [ 1 .. n ]
     |> List.map buzzer
 
 fizzbuzz 30
