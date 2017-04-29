4089beae6aa353ab8258d5b32713f2f658391934
Arne Lehmann <arne.lehmann@gmx.de>
2017-04-29 14:22:27 +0200

    fibonacci with caching of intermediate results

--- fibonacci.fsx
+++ fibonacci.fsx
@@ -2,9 +2,31 @@
 Fibonacci Sequence - Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
 *)
 
-let rec fib x = 
-    match x with
-    | 0 | 1 | 2 -> 1
-    | _ -> fib (x-1) + fib (x-2)
+open System
+open System.Collections.Generic
 
-let x = fib 8
+let cache = Dictionary<int64,int64>()
+cache.Add(0L, 1L)
+cache.Add(1L, 1L)
+cache.Add(2L, 1L)
+
+let rec fib x = 
+    let cached = cache.TryGetValue x
+    match cached with
+    | true, v ->
+        printfn "got cached result: %d" v
+        v
+    | false, _ -> 
+        let computed = 
+            match x with
+            | 0L | 1L | 2L -> 1L
+            | _ -> fib (x-1L) + fib (x-2L) 
+        printfn "computed new value: %d" computed
+        cache.Add(x, computed)
+        computed
+
+let numbers =
+    [ 1L .. 75L ]
+    |> Seq.map fib
+
+printfn "%s" (numbers |> Seq.map string |> String.concat ", ")
