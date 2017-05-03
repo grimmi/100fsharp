50e87f764f92bddd5ba29d55a674eb0f5c354c7d
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-03 21:53:36 +0200

    listreplication from hackerrank

---
+++ listreplication.fs
@@ -0,0 +1,24 @@
+(*
+    List Replication
+    Given a list, repeat each element in the list amount of times. 
+
+    taken from: https://www.hackerrank.com/challenges/fp-list-replication
+*)
+
+(*
+    input code for hackerrank:
+    let inputs = 
+    Seq.initInfinite (fun _ -> System.Console.ReadLine())
+    |> Seq.takeWhile (fun s -> s <> null)
+    |> Seq.map int
+    |> List.ofSeq
+    let s, xs = List.head inputs, List.tail inputs
+
+    repeatList xs s
+*)
+let repeatList list n = 
+    list
+    |> Seq.iter(fun x -> [ 1 .. n]
+                         |> Seq.iter(fun _ -> printfn "%d" x))
+
+let r = repeatList [1;2;3] 4
