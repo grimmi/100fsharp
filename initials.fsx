7fc387d14fb97440dc30fd8f50b884c920410862
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-05 20:29:23 +0200

    removed unneccessary type annotations

--- initials.fsx
+++ initials.fsx
@@ -3,8 +3,8 @@
     taken from: https://www.codewars.com/kata/57b56af6b69bfcffb80000d7/train/fsharp
 *)
 
-let toInitials (name:string) : string=
-    Seq.fold (fun (initials:string) (n:string) -> initials + " " + (n |> Seq.head |> string) + ".") "" (name.Split ' ')
+let toInitials (name:string) =
+    Seq.fold (fun initials n -> initials + " " + (n |> Seq.head |> string) + ".") "" (name.Split ' ')
 
 let i = "Jack Bauer" |> toInitials
 let r = "Robert C. Martin" |> toInitials    
