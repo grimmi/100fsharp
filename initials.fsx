0748118c20277ed6d5ed696c76bf01286c33f99b
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-05 20:33:09 +0200

    implemented initials both with Seq.map and Seq.fold

--- initials.fsx
+++ initials.fsx
@@ -4,7 +4,12 @@
 *)
 
 let toInitials (name:string) =
-    Seq.fold (fun initials n -> initials + " " + (n |> Seq.head |> string) + ".") "" (name.Split ' ')
+    (name.Split ' ')
+    |> Seq.map(fun part -> (part.[0] |> string) + ".")
+    |> String.concat " "
+
+    // let initials = Seq.fold (fun initials n -> initials + " " + (n |> Seq.head |> string) + ".") "" (name.Split ' ')
+    // initials.Trim()
 
 let i = "Jack Bauer" |> toInitials
 let r = "Robert C. Martin" |> toInitials    
