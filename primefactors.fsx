6eae55acc8d403e95343dcd9d01ba1c8116cf39d
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-06 14:58:28 +0200

    some trivial optimizations

--- primefactors.fsx
+++ primefactors.fsx
@@ -1,11 +1,14 @@
 let primefactors x = 
     let nextfactor y = 
-        let factor = [ 2 .. (y/2) ]
-                     |> Seq.skipWhile(fun idx -> y % idx <> 0)
-                     |> Seq.truncate 1
-        match factor |> Seq.length with
-        | 1 -> factor |> Seq.head
-        | _ -> y
+        if y % 2 = 0 then
+            2
+        else
+            let factor = [ 3 .. 2 .. int(ceil(sqrt(float(y)))) ]
+                         |> Seq.skipWhile(fun idx -> y % idx <> 0)
+                         |> Seq.truncate 1
+            match factor |> Seq.length with
+            | 1 -> factor |> Seq.head
+            | _ -> y
     
     let rec getfactors x fs =
         match nextfactor x with
@@ -14,11 +17,4 @@ let primefactors x =
         
     [] |> getfactors x
 
-// let z = primefactors 7
-// let z1 = primefactors 9
-// let z2 = primefactors 21
-// let z3 = primefactors 2
-// let z4 = primefactors 49
-// let z5 = primefactors 1
-
-let z6 = primefactors 65987
+let z6 = primefactors 656317973
