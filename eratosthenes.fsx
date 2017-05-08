5a060f2bdd22a142479db6c705d7f0059bf9e46f
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-08 19:03:44 +0200

    added module to sieve implementation

--- eratosthenes.fsx
+++ eratosthenes.fsx
@@ -2,6 +2,8 @@
     **Sieve of Eratosthenes** - 
     The sieve of Eratosthenes is one of the most efficient ways to find all of the smaller primes (below 10 million or so). 
 *)
+module Eratosthenes
+
 let processSieve (x:int) (sieve: (int * bool)[]) = 
     let limit = (sieve |> Seq.length) - 1
     let xLimit = pown (x |> int64) 2
@@ -26,9 +28,9 @@ let sieve x =
 
     sieve
 
-#time
-let sieved = sieve 10000000
-let primes = sieved |> Array.filter(fun (idx, prime) -> prime) |> Array.map(fun (idx, prime) -> idx)
-#time
+// #time
+// let sieved = sieve 10000000
+// let primes = sieved |> Array.filter(fun (idx, prime) -> prime) |> Array.map(fun (idx, prime) -> idx)
+// #time
 
-printfn "%d primzahlen gefunden" (primes |> Array.length)
+// printfn "%d primzahlen gefunden" (primes |> Array.length)
