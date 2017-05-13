db2b78f7a4ba7464b2cbe98a5c072b675d6fbea1
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-13 10:59:29 +0200

    added type for binary digits; implemented decimal -> binary

--- binarydecimal.fsx
+++ binarydecimal.fsx
@@ -2,14 +2,40 @@
     **Binary to Decimal and Back Converter** - Develop a converter to convert a decimal number to binary or a binary number to its decimal equivalent.
 *)
 
+type BinaryDigit =
+|BinaryOne
+|BinaryZero
+
 let binaryToDecimal (b:string) =
     b 
     |> Seq.rev
-    |> Seq.filter(fun c -> ['0';'1'] |> Seq.contains c)
-    |> Seq.mapi(fun idx c -> match c with
-                             |'0' -> 0
-                             |'1' -> pown 2 idx
-                             |_ -> 0)
+    |> Seq.choose(fun c -> match c with
+                           |'0' -> Some BinaryZero
+                           |'1' -> Some BinaryOne
+                           |_ -> None)
+    |> Seq.mapi(fun idx d -> match d with
+                             |BinaryZero -> 0
+                             |BinaryOne -> pown 2 idx)
     |> Seq.sum
 
 let b = "1010_0001" |> binaryToDecimal
+
+open System
+
+let decimalToBinary (c:int64) =
+    let maxlen = int((Math.Log(float(c), 2.)) + 1.)
+
+    let rec combineBinary acc pow current =
+        let currentPow = pown 2L pow
+        match pow with        
+        |(-1) -> acc
+        |_ when currentPow > current -> combineBinary (acc + "0") (pow - 1) current        
+        |_ -> combineBinary (acc + "1") (pow - 1) (current - currentPow)
+    
+    combineBinary "" maxlen c
+    |> Seq.skipWhile(fun c -> c = '0')
+    |> Seq.map string
+    |> String.concat ""
+
+let x = decimalToBinary 129L
+
