00e5422348843d13c254a6fb0243bd1177db777b
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-14 09:25:31 +0200

    added handling of negative numbers

--- stringtonumber.fsx
+++ stringtonumber.fsx
@@ -12,8 +12,12 @@ open System
 let stringToNumber str =
     let number, _ = str
                     |> Seq.rev
-                    |> Seq.map string
+                    |> Seq.takeWhile Char.IsDigit
+                    |> Seq.map string   // toString, because char |> int gives us the ascii code which we don't want
                     |> Seq.fold(fun (sum, factor) c -> (sum + (c |> int) * factor), factor * 10) (0, 1)
-    number
+    
+    match str |> Seq.head with
+    |'-' -> number * (-1)
+    |_ -> number
 
-let n = "1235" |> stringToNumber
+let n = "-1235" |> stringToNumber
