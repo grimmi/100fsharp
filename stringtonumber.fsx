5f198f1bcaa21ddd8c479f1045263f9b3bf2b6bb
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-14 09:39:52 +0200

    handling of numbers with decimal places

--- stringtonumber.fsx
+++ stringtonumber.fsx
@@ -8,16 +8,28 @@
 // let n = "12345" |> stringToNumber
 
 open System
+open System.Globalization
 
-let stringToNumber str =
-    let number, _ = str
-                    |> Seq.rev
-                    |> Seq.takeWhile Char.IsDigit
-                    |> Seq.map string   // toString, because char |> int gives us the ascii code which we don't want
-                    |> Seq.fold(fun (sum, factor) c -> (sum + (c |> int) * factor), factor * 10) (0, 1)
-    
+let stringToNumber (str:string) =
+    let toNumber text = 
+        let sum, _ = text
+                     |> Seq.rev
+                     |> Seq.takeWhile Char.IsDigit
+                     |> Seq.map string   // toString, because char |> int gives us the ascii code which we don't want
+                     |> Seq.fold(fun (sum, factor) c -> (sum + (c |> int) * factor), factor * 10) (0, 1)
+        sum
+
+    let numberParts = str.Split([|CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator|], StringSplitOptions.RemoveEmptyEntries)
+    let number = match numberParts with
+                 |[|f|] -> float(toNumber f)
+                 |[|f;s|] -> 
+                    let factor = pown 10. ((s |> Seq.length) * (-1))
+                    float(toNumber f) + (factor * float(toNumber s))
+                 |_ -> 0.
+
+
     match str |> Seq.head with
-    |'-' -> number * (-1)
+    |'-' -> number * (-1.)
     |_ -> number
 
-let n = "-1235" |> stringToNumber
+let n = "-123,45" |> stringToNumber
