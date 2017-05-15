5ada2622d02b518c4ebd4ba8efec41fd9d954050
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-15 19:32:52 +0200

    find second to last

---
+++ findsecondtolast.fsx
@@ -0,0 +1,22 @@
+(*
+    Problem 2 
+    Find the last but one element of a list. 
+    (Note that the Lisp transcription of this problem is incorrect.) 
+    Example in Haskell: 
+    Prelude> myButLast [1,2,3,4]
+    3
+    Prelude> myButLast ['a'..'z']
+    'y'
+*)
+
+let myButLast l =
+
+    let rec loop tail =
+        match tail with
+        |[x;y] -> Some x
+        |head::tail -> loop tail
+        |_ -> None
+
+    loop (l |> List.ofSeq)
+
+let s = "hello world" |> myButLast
